d454f933478a5237638eba1220282f837dde0d4e
mickaelandrianantenaina10-wq <mickaelandrianantenaina10@gmail.com>
2026-05-06 05:36:10 +0300

    Update TD3_ex9.c

--- TD3_ex9.c
+++ TD3_ex9.c
@@ -1,3 +1,31 @@
+/*
+ALGORITHME verifier_mot_carre
+VARIABLES
+    W : CHAÎNE DE CARACTÈRES (TAILLE MAX 50)
+    n, i, compteur : ENTIERS
+DÉBUT
+    AFFICHER("Entrer un mot pour verifier s'il est carre :")
+    SAISIR W
+    n ← LONGUEUR(W)
+    SI n ≤ 2 ALORS
+        AFFICHER("Le mot n'est pas carre \n")
+    SINON SI n MODULO 2 ≠ 0 ALORS
+        AFFICHER("Le mot n'est pas carre\n")
+    SINON
+        compteur ← 0
+        POUR i DE 0 À (n/2 - 1) FAIRE
+            SI W[i] ≠ W[i + n/2] ALORS
+                compteur ← compteur + 1
+            FIN SI
+        FIN POUR
+        SI compteur = 0 ALORS
+            AFFICHER("Le mot est carre\n")
+        SINON
+            AFFICHER("Le mot n'est pas carre\n")
+        FIN SI
+    FIN SI
+FIN
+*/
 #include<stdio.h>
 void afficher(char mot[]);
 void prendre(char mot[]);
