321e0a672460b871117cf48c542eda6a9c40611f
mickaelandrianantenaina10-wq <mickaelandrianantenaina10@gmail.com>
2026-05-06 05:40:10 +0300

    Update TD3_ex10.c

--- TD3_ex10.c
+++ TD3_ex10.c
@@ -1,3 +1,33 @@
+/*
+ALGORITHME
+VARIABLES
+    lettre : CHAÎNE DE CARACTÈRES (TAILLE MAX 500)
+    conservateur : CHAÎNE DE CARACTÈRES (TAILLE MAX 500)
+    i, compteur, a : ENTIERS
+DÉBUT
+    AFFICHER("Entrer une lettre :")
+    SAISIR lettre
+    compteur ← 0
+    TANT QUE lettre[compteur] ≠ '\0' FAIRE
+        compteur ← compteur + 1
+    FIN TANT QUE
+    a ← 0
+    POUR i DE 0 À compteur-1 FAIRE
+        SI lettre[i] = 'a' OU lettre[i] = 'e' OU lettre[i] = 'i' OU lettre[i] = 'o' OU lettre[i] = 'u' OU lettre[i] = 'y' ALORS
+            conservateur[a] ← lettre[i]
+            conservateur[a+1] ← 'I'
+            conservateur[a+2] ← 'T'
+            a ← a + 3
+        SINON
+            conservateur[a] ← lettre[i]
+            a ← a + 1
+        FIN SI
+    FIN POUR
+    conservateur[a] ← '\0'
+    AFFICHER(conservateur)
+    AFFICHER("\n")
+FIN
+*/
 #include<stdio.h>
 int main()
 {
@@ -10,10 +40,6 @@ int main()
 	{
 		 	compteur++;
 	}
-	/*for (i=0;i<compteur;i++)
-	{
-		conservateur[i]=lettre[i];
-	}*/
 	a=0;
 	for (i=0;i<compteur;i++)
 	{
